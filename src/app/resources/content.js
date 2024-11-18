968e7c818f398742ec6874d3f015f801939022b7
Fahmi Aquinas <fahmi.aquinas@gmail.com>
2024-11-18 21:02:27 +0700

    add role

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -6,7 +6,7 @@ const person = {
     get name() {
         return `${this.firstName} ${this.lastName}`;
     },
-    role:      '',
+    role:      'Software Engineer',
     avatar:    '/images/avatar.jpg',
     location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
