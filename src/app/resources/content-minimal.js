9ac09acc643cf6aa350ddf2e8ae38e3edd1ee7f3
Fahmi Aquinas <fahmi.aquinas@gmail.com>
2026-06-22 22:54:16 +0700

    change photo

--- src/app/resources/content-minimal.js
+++ src/app/resources/content-minimal.js
@@ -8,7 +8,7 @@ const person = {
         return `${this.firstName} ${this.lastName}`;
     },
     role:      'Software Engineer',
-    avatar:    '/images/avatar-cgt.png',
+    avatar:    '/images/avatar-hero.png',
     location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
 }
